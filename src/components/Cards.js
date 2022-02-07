388a691ea539b9734857112c61626aa53efa75fb
Florid11 <florid.arifaj@hotmail.com>
2022-02-07 19:35:33 +0100

    Card branch created

---
+++ src/components/Cards.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Cards.module.css";
+
+const cards = () => {
+  return(
+      <div className={classes.cardBox}>
+          <h1>Hello</h1>
+      </div>
+  );
+}
+
+export default cards;
