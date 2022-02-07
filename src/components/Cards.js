69e72f54e689d8be198604f5f87fa94495ba59f4
Florid11 <florid.arifaj@hotmail.com>
2022-02-07 19:53:49 +0100

    Map function created

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,78 @@
 import React from "react";
 import classes from "./Cards.module.css";
+import Tabs from "../tabsNavigation/Tabs";
 
 const cards = () => {
+
+    const cardData = [
+        {
+            id:'0',
+            name: '.COM',
+            price: '$5.99/yr',
+            description: 'Instead of $10.99/yr'
+        },
+        {
+            id:'1',
+            name: '.AI',
+            price: '$5.99/yr',
+            description: 'Instead of $10.99/yr'
+        },
+        {
+            id:'2',
+            name: '.NET',
+            price: '$7.99/yr',
+            description: 'Instead of $10.99/yr'
+        },
+        {
+            id:'3',
+            name: '.HEALTH',
+            price: '$7.99/yr',
+            description: 'Instead of $10.99/yr'
+        },
+        {
+            id:'4',
+            name: '.CO.UK',
+            price: '$3.99/yr',
+            description: 'Instead of $10.99/yr'
+        },
+        {
+            id:'5',
+            name: '.ORG',
+            price: '$15.99/yr',
+            description: 'Instead of $19.99/yr'
+        },
+        {
+            id:'6',
+            name: '.CO',
+            price: '$26.33/yr',
+            description: 'Instead of $29.99/yr'
+        },
+        {
+            id:'7',
+            name: '.SEA',
+            price: '$26.33/yr',
+            description: 'Instead of $29.99/yr'
+        },
+
+    ];
   return(
       <div className={classes.cardBox}>
-          <h1>Hello</h1>
+          {cardData.map((data) =>{
+              return(
+                  <div className={classes.dataBox}>
+                      <div>
+                          <h1>{data.name}</h1>
+                      </div>
+                      <div>
+                        <h3>{data.price}</h3>
+                          <p>{data.description}</p>
+                      </div>
+                      <div>
+                        <button className={classes.dwn_btn}>Buy Now</button>
+                      </div>
+                  </div>
+              );
+          })}
       </div>
   );
 }
