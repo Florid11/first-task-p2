d78a9849ee693af14a3832a83970c3708de189c3
Florid11 <florid.arifaj@hotmail.com>
2022-02-07 19:32:36 +0100

    Changes made to tabs

--- src/tabsNavigation/Tabs.js
+++ src/tabsNavigation/Tabs.js
@@ -1,10 +1,28 @@
 import React from "react";
 import classes from "./Tabs.module.css";
+import {Tab,Tabs,TabList,TabPanel} from "react-tabs";
 
 const tabsComponent = () => {
+
   return(
       <div className={classes.box}>
-          <h1>Hi</h1>
+          <div className={classes.tab}>
+              <Tabs>
+                  <TabList className={classes.tab_btn}>
+                      <Tab className={classes.tab_click}>Domains</Tab>
+                      <Tab disabled className={classes.tab_click}>Web Hosting</Tab>
+                      <Tab disabled className={classes.tab_click}>Dedicated Servers</Tab>
+                      <Tab disabled className={classes.tab_click}>Virtual Cloud Servers</Tab>
+                      <Tab disabled className={classes.tab_click}>WordPress Hosting</Tab>
+                      <Tab disabled className={classes.tab_click}>Email Hosting</Tab>
+                      <Tab disabled className={classes.tab_click}>VPS Hosting Servers</Tab>
+                      <Tab disabled className={classes.tab_click}>Free Hosting</Tab>
+                  </TabList>
+                  <TabPanel className={classes.panel}>
+                      <p>Hello there</p>
+                  </TabPanel>
+              </Tabs>
+          </div>
       </div>
   );
 }
