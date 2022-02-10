dce0d5e419a55f8c0a80a528a883e573c81c74ea
Florid11 <florid.arifaj@hotmail.com>
2022-02-10 21:00:38 +0100

    Topics Created

---
+++ src/components/Topics.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Topics.module.css";
+
+const Topic = () => {
+  return(
+    <div>
+        <h1 className={classes.about}>About us</h1>
+    </div>
+  );
+}
+
+export default Topic;
