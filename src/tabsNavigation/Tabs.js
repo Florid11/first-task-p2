c00f950415b175850c78a0c95eace48ec719c1cc
Florid11 <florid.arifaj@hotmail.com>
2022-02-10 11:54:39 +0100

    Final changes made to tabs

--- src/tabsNavigation/Tabs.js
+++ src/tabsNavigation/Tabs.js
@@ -1,26 +1,47 @@
 import React from "react";
 import classes from "./Tabs.module.css";
 import {Tab,Tabs,TabList,TabPanel} from "react-tabs";
-import cards from "../components/Cards"
+import cards from "../components/Cards";
+import "./Tabs.css";
 
 const tabsComponent = () => {
-    const cardsData = cards();
   return(
       <div className={classes.box}>
           <div className={classes.tab}>
               <Tabs>
                   <TabList className={classes.tab_btn}>
-                      <Tab className={classes.tab_click}>Domains</Tab>
-                      <Tab disabled className={classes.tab_click}>Web Hosting</Tab>
-                      <Tab disabled className={classes.tab_click}>Dedicated Servers</Tab>
-                      <Tab disabled className={classes.tab_click}>Virtual Cloud Servers</Tab>
-                      <Tab disabled className={classes.tab_click}>WordPress Hosting</Tab>
-                      <Tab disabled className={classes.tab_click}>Email Hosting</Tab>
-                      <Tab disabled className={classes.tab_click}>VPS Hosting Servers</Tab>
-                      <Tab disabled className={classes.tab_click}>Free Hosting</Tab>
+                      <Tab >Domains</Tab>
+                      <Tab disabled >Web Hosting</Tab>
+                      <Tab disabled >Dedicated Servers</Tab>
+                      <Tab disabled >Virtual Cloud Servers</Tab>
+                      <Tab disabled >WordPress Hosting</Tab>
+                      <Tab disabled >Email Hosting</Tab>
+                      <Tab disabled >VPS Hosting Servers</Tab>
+                      <Tab disabled >Free Hosting</Tab>
                   </TabList>
                   <TabPanel className={classes.panel}>
-                      {cardsData}
+                      {cards()}
+                  </TabPanel>
+                  <TabPanel className={classes.panel}>
+                      <h1>Hello</h1>
+                  </TabPanel>
+                  <TabPanel className={classes.panel}>
+                      <h1>Hello</h1>
+                  </TabPanel>
+                  <TabPanel className={classes.panel}>
+                      <h1>Hello</h1>
+                  </TabPanel>
+                  <TabPanel className={classes.panel}>
+                      <h1>Hello</h1>
+                  </TabPanel>
+                  <TabPanel className={classes.panel}>
+                      <h1>Hello</h1>
+                  </TabPanel>
+                  <TabPanel className={classes.panel}>
+                      <h1>Hello</h1>
+                  </TabPanel>
+                  <TabPanel className={classes.panel}>
+                      <h1>Hello</h1>
                   </TabPanel>
               </Tabs>
           </div>
