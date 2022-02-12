72423e224ae487250eae2c6ceb5208f11c7f7cdc
Florid11 <florid.arifaj@hotmail.com>
2022-02-12 10:57:06 +0100

    Changes made to topics

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,10 +1,34 @@
 import React from "react";
-import classes from "./Topics.module.css";
+import ToggleState from "../ToggleTopics/Toggle";
 
 const Topic = () => {
+    const topics =[
+        {
+            key: '1',
+            title: 'How does ParkName seperate itself from other domain name parking companies?',
+            paragraphs: 'Your domains are a valuable online property. As in any investment, you want the most efficient, easy way to make sure your property is going to be profitable. Do you own more than 1,000 domains? As a professional domainer, you will find everything you need through Parkname to generate maximum profits from your domain portfolio.'
+        },
+        {
+            key: '2',
+            title: 'Is ParkName Parking actually free?',
+            paragraphs: 'Your domains are a valuable online property. As in any investment, you want the most efficient, easy way to make sure your property is going to be profitable. Do you own more than 1,000 domains? As a professional domainer, you will find everything you need through Parkname to generate maximum profits from your domain portfolio.'
+        },
+        {
+            key: '3',
+            title: 'What you do?',
+            paragraphs: 'Your domains are a valuable online property. As in any investment, you want the most efficient, easy way to make sure your property is going to be profitable. Do you own more than 1,000 domains? As a professional domainer, you will find everything you need through Parkname to generate maximum profits from your domain portfolio.'
+        },
+        {
+            key: '4',
+            title: 'When was Parkname first founded?',
+            paragraphs: 'Your domains are a valuable online property. As in any investment, you want the most efficient, easy way to make sure your property is going to be profitable. Do you own more than 1,000 domains? As a professional domainer, you will find everything you need through Parkname to generate maximum profits from your domain portfolio.'
+        },
+        ]
   return(
     <div>
-        <h1 className={classes.about}>About us</h1>
+        {topics.map((topic =>
+        <ToggleState key={topic.key} title={topic.title} paragraphs={topic.paragraphs} />
+        ))}
     </div>
   );
 }
