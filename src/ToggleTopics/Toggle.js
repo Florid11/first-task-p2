9d9fe28d2bbcde033ebd2d01e696cbd95f7ff0bb
Florid11 <florid.arifaj@hotmail.com>
2022-02-14 17:13:18 +0100

    Map functions created

--- src/ToggleTopics/Toggle.js
+++ src/ToggleTopics/Toggle.js
@@ -5,14 +5,33 @@ import classes from "./Toggle.module.css";
 
 const ToggleState = (props) => {
     const [toggle, setToggle] = useState(false);
-
-    const toggler = () => {
-        setToggle((prev) => !prev)
-    }
+    const [pToggle, setPToggle] = useState(false);
+    const topics =[
+        {
+            key: '1',
+            title: 'How does ParkName seperate itself from other domain name parking companies?',
+            paragraphs: 'Your domains are a valuable online property. As in any investment, you want the most efficient, easy way to make sure your property is going to be profitable. Do you own more than 1,000 domains? As a professional domainer, you will find everything you need through Parkname to generate maximum profits from your domain portfolio.'
+        },
+        {
+            key: '2',
+            title: 'Is ParkName Parking actually free?',
+            paragraphs: 'Your domains are a valuable online property. As in any investment, you want the most efficient, easy way to make sure your property is going to be profitable. Do you own more than 1,000 domains? As a professional domainer, you will find everything you need through Parkname to generate maximum profits from your domain portfolio.'
+        },
+        {
+            key: '3',
+            title: 'What you do?',
+            paragraphs: 'Your domains are a valuable online property. As in any investment, you want the most efficient, easy way to make sure your property is going to be profitable. Do you own more than 1,000 domains? As a professional domainer, you will find everything you need through Parkname to generate maximum profits from your domain portfolio.'
+        },
+        {
+            key: '4',
+            title: 'When was Parkname first founded?',
+            paragraphs: 'Your domains are a valuable online property. As in any investment, you want the most efficient, easy way to make sure your property is going to be profitable. Do you own more than 1,000 domains? As a professional domainer, you will find everything you need through Parkname to generate maximum profits from your domain portfolio.'
+        },
+    ]
 
   return(
     <div className={classes.boxtopic}>
-        <div>
+        <div className={classes.about}>
             <div>
                 <p className={classes.user}><FaRegUserCircle /></p>
             </div>
@@ -20,6 +39,18 @@ const ToggleState = (props) => {
                 <h1>About Us</h1>
                 <p>4 articles in this Topic</p>
             </div>
+            <button onClick={()=>setToggle(!toggle)}><FaAngleRight /></button>
+            {
+                toggle ? (topics.map(topic =>{
+                    return(
+                        <div>
+                            {topic.title}
+                            <button onClick={()=>setPToggle(!pToggle)}><FaAngleRight /></button>
+                            {pToggle ? topic.paragraphs : null}
+                        </div>
+                    )
+                })) : null
+            }
         </div>
 
     </div>
